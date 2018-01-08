ed867aa88c1529605e91aa7178afd1ec57b57f9a
Amruta Hoke <Amruta.Hoke@Staples.com>
2018-01-08 23:00:17 +0530

    added message

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('NEW user connected');
 
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+    socket.emit('newMessage', generateMessage('Amruta', 'Welcome Pradeep'));
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
