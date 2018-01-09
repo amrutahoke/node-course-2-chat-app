1c95a2507c29267ca55920629898df07d29bbfb2
Amruta Hoke <Amruta.Hoke@Staples.com>
2018-01-09 15:11:49 +0530

    format for timestamp

--- public/js/index.js
+++ public/js/index.js
@@ -11,14 +11,16 @@ socket.on('disconnect', function() {
 socket.on('newMessage', function (message) {
     console.log('NEW message', message);
     var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    li.text(`${message.from} ${formattedTime}: ${message.text}`);
     jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
    var li = jQuery('<li></li>');
+   var formattedTime = moment(message.createdAt).format('h:mm a');
    var a = jQuery('<a target="_blank">My Current Location</a>');
-   li.text(`${message.from}: `);
+   li.text(`${message.from} ${formattedTime}: `);
    a.attr('href', message.url);
    li.append(a);
    jQuery('#messages').append(li);
